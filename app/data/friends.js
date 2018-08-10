367291f486ba5370ee28f7a6374468032cad8423
Honey B <34516348+elizabethjmz@users.noreply.github.com>
2018-08-10 13:18:08 -0500

    Working version for Friend Finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,24 @@
 // Results
 
 var friendsList = [ {
-    name: "Test User 1",
-    pic_url: "http://adgsystems.com.do/img/placeholder.jpg",
-    scores: [5,3,3,5,1,1,3,2,2,4]
+        name: "John",
+        pic_url: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [5,3,3,5,1,1,3,2,2,4]
+    },
+    {
+        name: "Sarah",
+        pic_url: "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [3,3,1,5,5,1,1,5,2,3]
+    },
+    {
+        name: "Valerie",
+        pic_url: "https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [5,5,5,5,5,5,5,5,4,4]
+    },
+    {
+        name: "Peter Lee",
+        pic_url: "https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [1,1,1,5,5,3,3,5,2,4]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,18 +4,26 @@
 
 var friendsList = require("../data/friends.js");
 
-
 module.exports = function(app){
     //Display API for All Friends
     app.get("/api/friends_list", function(req, res) {
+        /*
+        for (var n = 0; n < res.length; n++){
+            //console.log("Inside for");
+            //console.log(res[n].scores);
+            
+        }
+        */
+        //console.log ("From API\n"+res);
         return res.json(friendsList); 
     });
     //Display waitinglist API
     app.post("/api/friends_list", function(req, res) {
-        
+        //console.log (req);
         var name = req.body.name;
         var photo = req.body.pic_url;
         var user_scores = [];
+        
         for (var k=0; k< (req.body.scores).length; k++) {
             user_scores.push(parseInt(req.body.scores[k]));
         }
@@ -26,6 +34,6 @@ module.exports = function(app){
         }
         friendsList.push(new_obj);
         //console.log(new_obj);
-        console.log(friendsList);
+        //console.log(friendsList);
     });
 };
